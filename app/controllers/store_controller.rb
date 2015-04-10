6e65cbc997080bbf50875f136b88803cc6a53ef7
libochao <522427432@qq.com>
2015-04-10 16:57:19 +0800

    4-10 mission f

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -17,7 +17,7 @@
 class StoreController < ApplicationController
   def index
     @products = Product.all
-	
+    @cart = current_cart
   end
 
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -15,8 +15,14 @@
 # Visit http://www.pragmaticprogrammer.com/titles/rails4 for more book information.
 #---
 class Product < ActiveRecord::Base
-  #default_scope {select(:order => 'title')}
-  #default_scope :order => 'title'
+  default_scope {order(:title)}
+  has_many :line_items
+
+  before_destroy :ensure_not_referenced_by_any_line_item
+
+  #...
+
+
   # validation stuff...
 
 
@@ -29,4 +35,15 @@ class Product < ActiveRecord::Base
     :message => 'must be a URL for GIF, JPG or PNG image.'
   }
   validates :title, :length => {:minimum => 10}
+  private
+
+    # ensure that there are no line items referencing this product
+    def ensure_not_referenced_by_any_line_item
+      if line_items.empty?
+        return true
+      else
+        errors.add(:base, 'Line Items present')
+        return false
+      end
+    end
 end
